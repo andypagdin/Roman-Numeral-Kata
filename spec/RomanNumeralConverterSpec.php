ad6c94185e5e694bfe6a44ca7840396b57c8b6cc
andypagdin <andyjpagdin@gmail.com>
2018-12-04 20:44:58 +0000

    Add more values to the lookup array
    This will allow the convert method to handle all possible valid numbers.

--- spec/RomanNumeralConverterSpec.php
+++ spec/RomanNumeralConverterSpec.php
@@ -21,4 +21,18 @@ class RomanNumeralConverterSpec extends ObjectBehavior
     function it_calculates_the_roman_numeral_for_1600() {
         $this->convert(1600)->shouldReturn('MDC');
     }
+
+    /**
+     * Test against edge case.
+     */
+    function it_calculates_the_roman_numeral_for_1999() {
+        $this->convert(1999)->shouldReturn('MCMXCIX');
+    }
+
+    /**
+     * Test a larger number.
+     */
+    function it_calculates_the_roman_numeral_for_3999() {
+        $this->convert(3999)->shouldReturn('MMMCMXCIX');
+    }
 }
--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -14,9 +14,13 @@ class RomanNumeralConverter implements RomanNumeral
      */
     protected static $lookup = [
         1000 => 'M',
+        900 => 'CM',
         500 => 'D',
+        400 => 'CD',
         100 => 'C',
+        90 => 'XC',
         50 => 'L',
+        40 => 'XL',
         10 => 'X',
         9 => 'IX',
         5 => 'V',
