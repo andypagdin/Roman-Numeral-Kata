8eba323c44855f9faf4b498404969d045228e234
andypagdin <andyjpagdin@gmail.com>
2018-12-04 20:34:59 +0000

    Define lookup array and refactor convert method

--- spec/RomanNumeralConverterSpec.php
+++ spec/RomanNumeralConverterSpec.php
@@ -8,7 +8,17 @@ use Prophecy\Argument;
 
 class RomanNumeralConverterSpec extends ObjectBehavior
 {
+    /**
+     * Test a basic integer.
+     */
     function it_calculates_the_roman_numeral_for_1() {
         $this->convert(1)->shouldReturn('I');
+    }
+
+    /**
+     * Test an integer than will return more than one numeral.
+     */
+    function it_calculates_the_roman_numeral_for_1600() {
+        $this->convert(1600)->shouldReturn('MDC');
     }
 }
--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -7,7 +7,33 @@ interface RomanNumeral
 
 class RomanNumeralConverter implements RomanNumeral
 {
+    /**
+     * Define the roman numeral lookup array, in decending order by numeral value.
+     * 
+     * @var array
+     */
+    protected static $lookup = [
+        1000 => 'M',
+        500 => 'D',
+        100 => 'C',
+        50 => 'L',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I'
+    ];
+
     public function convert(int $number) {
-        return 'I';
+        $result = '';
+
+        foreach (static::$lookup as $key => $value) {
+            while ($number >= $key) {
+                $number -= $key;
+                $result .= $value;
+            }
+        }
+
+        return $result;
     }
 }
